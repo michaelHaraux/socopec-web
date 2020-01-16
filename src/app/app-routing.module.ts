044f70da412e557803f33b0879caf6a06fcd9737
michaelHaraux <35221040+michaelHaraux@users.noreply.github.com>
2020-01-16 13:32:35 +0100

    test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AGENTComponent } from 'D:/RIL/Projet Web - Alexandre DE BEUKELAER/Projet Angular/SOCOPEC/src/app/agent/agent.component';
+import { AGENTComponent } from './agent/agent.component';
 
 const routes: Routes = [
   { path: 'agents', component: AGENTComponent },
