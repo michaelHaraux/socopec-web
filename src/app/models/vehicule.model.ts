ea27ea06c879ffa8c2f23fe16d7dc2a09f2b21bd
michaelHaraux <35221040+michaelHaraux@users.noreply.github.com>
2020-01-31 18:17:02 +0100

    Vehicule : ajout des champs

--- src/app/models/vehicule.model.ts
+++ src/app/models/vehicule.model.ts
@@ -1,6 +1,14 @@
+import { Agence } from './agence.model';
+
 export class Vehicule {
 
     photo: string;
+    dateFab : string;
+    hauteur : string;
+    largeur : string;
+    poids: number;
+    puissance : number;
+    agence : string;
     
     constructor(public identifiant: string, public modele: string) {
     }
--- src/app/vehicule-list/vehicule-form/vehicule-form.component.ts
+++ src/app/vehicule-list/vehicule-form/vehicule-form.component.ts
@@ -4,6 +4,7 @@ import { Vehicule } from '../../models/vehicule.model';
 import { VehiculesService } from '../../services/vehicule.service';
 import { Router } from '@angular/router';
 import * as firebase from 'firebase';
+import { Agence } from 'src/app/models/agence.model';
 
 @Component({
   selector: 'app-vehicule-form',
@@ -17,7 +18,7 @@ export class VehiculeFormComponent implements OnInit {
   fileIsUploading = false;
   fileUrl: string;
   fileUploaded = false;
-  //add =false;
+  
   
 
   constructor(private formBuilder: FormBuilder, private vehiculesService: VehiculesService,
@@ -26,23 +27,36 @@ export class VehiculeFormComponent implements OnInit {
   ngOnInit() {
     this.initForm();
   }
-  // toggleVisibility(e){
-  //   this.add= e.target.checked;
-  // }
+
   initForm() {
     this.vehiculeForm = this.formBuilder.group({
       identifiant: ['', Validators.required],
       modele: ['', Validators.required],
-
+      dateFab: '',
+      hauteur:'',
+      largeur:'',
+      poids:'',
+      puissance:'',
+      agence : '',
     });
   }
   
   onSaveVehicule() {
     const identifiant = this.vehiculeForm.get('identifiant').value;
     const modele = this.vehiculeForm.get('modele').value;
- 
-    const newVehicule = new Vehicule(identifiant, modele);
+    const dateFab = this.vehiculeForm.get('dateFab').value;
+    const hauteur = this.vehiculeForm.get('hauteur').value;
+    const largeur = this.vehiculeForm.get('largeur').value;
+    const poids = this.vehiculeForm.get('poids').value;
+    const puissance = this.vehiculeForm.get('puissance').value;
 
+    const newVehicule = new Vehicule(identifiant, modele);
+    newVehicule.dateFab = dateFab;
+    newVehicule.hauteur = hauteur;
+    newVehicule.largeur = largeur;
+    newVehicule.poids = poids;
+    newVehicule.puissance = puissance;
+    newVehicule.dateFab = dateFab;
 
     if(this.fileUrl && this.fileUrl !== '') {
       newVehicule.photo = this.fileUrl;
