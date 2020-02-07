04ef34e8817452a3790df415406922e6818c945c
michaelHaraux <35221040+michaelHaraux@users.noreply.github.com>
2020-02-07 16:12:30 +0100

    Export BackupList Excel

--- src/app/vehicule-list/backup-vehicule/backup-vehicule.component.ts
+++ src/app/vehicule-list/backup-vehicule/backup-vehicule.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit ,ElementRef, ViewChild  } from '@angular/core';
 import { VehiculesService } from '../../services/vehicule.service';
 import { Vehicule } from '../../models/vehicule.model';
 import { Subscription } from 'rxjs';
 import { Router } from '@angular/router';
 import { vehiculeBackup } from '../../models/vehiculeBackup.models';
-
+import * as XLSX from 'xlsx'; 
 
 @Component({
   selector: 'app-backup-vehicule',
@@ -13,6 +13,7 @@ import { vehiculeBackup } from '../../models/vehiculeBackup.models';
 })
 export class BackupVehiculeComponent implements OnInit, OnDestroy {
 
+  
   vehiculesBackup: vehiculeBackup[];
   vehiculesSubscription: Subscription;
   
@@ -27,6 +28,14 @@ export class BackupVehiculeComponent implements OnInit, OnDestroy {
     );
     this.vehiculesService.emitVehiculesBackup();
   }
+  @ViewChild('TABLE', { static: false }) TABLE: ElementRef;  
+  title = 'Excel';  
+  ExportTOExcel() {  
+    const ws: XLSX.WorkSheet = XLSX.utils.table_to_sheet(this.TABLE.nativeElement);  
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();  
+    XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');  
+    XLSX.writeFile(wb, 'ScoreSheet.xlsx');  
+  } 
   ngOnDestroy() {
     this.vehiculesSubscription.unsubscribe();
   }
