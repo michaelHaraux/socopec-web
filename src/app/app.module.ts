90b06c558f8d054aed29a3e3048e3e7a7c4a1044
michaelHaraux <35221040+michaelHaraux@users.noreply.github.com>
2020-02-11 17:30:35 +0100

    Correction node_modules angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -29,6 +29,8 @@ import { EditAgentComponent } from './agent-list/edit-agent/edit-agent.component
 import { EditAgenceComponent } from './agence-list/edit-agence/edit-agence.component';
 import { BackupVehiculeComponent } from './vehicule-list/backup-vehicule/backup-vehicule.component';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
+//import { FilterPipeModule } from 'ngx-filter-pipe';
+
 registerLocaleData(localeFr);
 
 @NgModule({
@@ -53,6 +55,7 @@ registerLocaleData(localeFr);
     BackupVehiculeComponent
   ],
   imports: [
+  //  FilterPipeModule,
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
